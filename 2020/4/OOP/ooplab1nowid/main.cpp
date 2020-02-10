af8cae271b40950af31a12d622d6ddf263813717
blackdrako <blackdrako@users.noreply.github.com>
2020-02-10 11:24:25 +0300

    123

--- 2020/4/OOP/ooplab1nowid/main.cpp
+++ 2020/4/OOP/ooplab1nowid/main.cpp
@@ -1,7 +1,113 @@
 #include <QCoreApplication>
 #include <QTextCodec>
 #include <QTextStream>
-#include <task.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+void creat_file_task1(string path){
+    ofstream fout;
+    fout.open(path);//для бинарника доп
+    if(!fout.is_open()){
+        cout << "Ошибка открытия файла!" << endl;
+    } else {
+        cout << "Count number:";
+        int x;
+        cin >> x;
+        for (int i=0;i<x;i++) {
+            string str;
+            cin >> str;
+            fout << str << endl;
+        }
+    }
+    fout.close();
+}
+
+void task1pod(string path){
+    ifstream fin;
+    fin.open(path);
+    if(!fin.is_open()){
+        cout << "Error file" << endl;
+    }else {
+        string str;
+        char temp[255];
+        string::iterator it;
+        while (fin.getline(temp,255)) {
+            str = temp;
+            it = str.begin();
+            cout << *(it);
+        }
+    }
+    fin.close();
+}
+
+void task1(){
+    string path1 = "taskout11.txt";
+    string path2 = "taskout12.txt";
+    int flag = 0;
+    cin >> flag;
+    if(flag!=0){
+        creat_file_task1(path1);
+        creat_file_task1(path2);
+    }
+    task1pod(path1);
+    task1pod(path2);
+}
+
+void creat_file_task2(){
+    ofstream foutf;
+    foutf.open("taskout2f.txt");
+    ofstream foutg;
+    foutg.open("taskout2g.txt");
+    if(!foutf.is_open()){
+        cout << "Ошибка открытия файла!" << endl;
+    } else {
+        cout << "Count number:";
+        int x;
+        cin >> x;
+        cout << "File f:";
+        int temp;
+        for (int i=0; i < x; i++) {
+            cin >> temp;
+            foutf << temp;
+            if(i < x-1)
+                foutf << endl;
+        }
+        cout << "File g:";
+        for (int i=0; i < x; i++) {
+            cin >> temp;
+            foutg << temp;
+            if(i < x-1)
+                foutg << endl;
+        }
+    }
+    foutf.close();
+    foutg.close();
+}
+
+void task2(){
+    ifstream finf;
+    ifstream fing;
+    ofstream fout;
+    finf.open("taskout2f.txt");
+    fing.open("taskout2g.txt");
+    fout.open("tempname.txt");
+    int tempf;
+    int tempg;
+    while (!finf.eof()) {
+        finf >> tempf;
+        fing >> tempg;
+        if(tempf > tempg)
+            fout << tempf;
+    }
+    finf.close();
+    fing.close();
+    fout.close();
+    remove("taskout2f.txt");
+    rename("tempname.txt","taskout2f.txt");
+}
+
 int main(int argc, char *argv[])
 {
 #ifdef Q_OS_WIN32
@@ -12,7 +118,8 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 #endif
     QCoreApplication app(argc, argv);
-    task Q1(1);
-    task Q2(2);
+    //task1();
+    //creat_file_task2();
+    task2();
     return app.exec();
 }
--- 2020/4/OOP/ooplab1nowid/ooplab1nowid.pro
+++ 2020/4/OOP/ooplab1nowid/ooplab1nowid.pro
@@ -15,15 +15,11 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        main.cpp \
-        task.cpp \
-        task1.cpp \
-        task2.cpp
+        main.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
-HEADERS += \
-    task.h
+HEADERS +=
