b64ab506a34682e7fbd93605d8d5eee0db7a75f0
blackdrako <kovalev_paha@mail.ru>
2020-10-09 10:34:25 +0300

    1

---
+++ TFL_lab2/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+#include <QTextCodec>
+#include <QTextStream>
+
+int main(int argc, char *argv[])
+{
+#ifdef Q_OS_WIN32
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("IBM 866"));
+#endif
+
+#ifdef Q_OS_LINUX
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#endif
+    QCoreApplication app(argc, argv);
+
+    return app.exec();
+}
